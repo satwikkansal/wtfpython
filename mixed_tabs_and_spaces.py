a6ce6259faebf039c95191c1fcb9c20e8ed1b2fc
cclauss <cclauss@bluewin.ch>
2017-09-04 14:56:27 +0200

    Mark TabError as noqa to silence linters on Python 3

--- mixed_tabs_and_spaces.py
+++ mixed_tabs_and_spaces.py
@@ -2,6 +2,6 @@ def square(x):
     sum_so_far = 0
     for counter in range(x):
         sum_so_far = sum_so_far + x
-	return sum_so_far
+	return sum_so_far  # noqa Python 3 will raise a TabError here
 
 print(square(10))

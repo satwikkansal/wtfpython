c0a81511532c22ec0bf18b7f5b9670b6e5d11f4e
cclauss <cclauss@bluewin.ch>
2017-09-04 18:42:42 +0200

    # noqa: E999 # pylint: disable=bad-indentation

--- mixed_tabs_and_spaces.py
+++ mixed_tabs_and_spaces.py
@@ -2,6 +2,6 @@ def square(x):
     sum_so_far = 0
     for counter in range(x):
         sum_so_far = sum_so_far + x
-	return sum_so_far  # noqa Python 3 will raise a TabError here
+	return sum_so_far  # noqa: E999 # pylint: disable=bad-indentation Python 3 will raise a TabError here
 
 print(square(10))

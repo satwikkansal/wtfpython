49ed35e0996f2c5af725970a0499807fe4697299
Satwik <satwikkansal@gmail.com>
2019-12-21 00:16:19 +0530

    Add hosted notebook instructions

--- irrelevant/notebook_generator.py
+++ irrelevant/notebook_generator.py
@@ -32,6 +32,27 @@ current_section_name = ""
 
 STATEMENT_PREFIXES = ["...", ">>> ", "$ "]
 
+HOSTED_NOTEBOOK_INSTRUCTIONS = """
+
+# Hosted notebook instructions
+
+This is just an experimental attempt of browsing wtfpython through jupyter notebooks. Some examples are read-only because, 
+- they either require a version of Python that's not supported in the hosted runtime.
+- or they can't be reproduced in the notebook envrinonment.
+
+The expected outputs are already present in collapsed cells following the code cells. The Google colab provides Python2 (2.7) and Python3 (3.6, default) runtimes. You can switch among these for Python2 specific examples. For examples specific to other minor versions, you can simply refer to collapsed outputs (it's not possible to control the minor version in hosted notebooks as of now). You can check the active version using
+
+```py
+>>> import sys
+>>> sys.version
+# Prints out Python version here.
+```
+
+That being said, most of tbe examples do work as expected. If you face any trouble, feel free to consult the original content on wtfpython and create an issue in the repo. Have fun!
+
+---
+"""
+
 
 def generate_code_block(statements, output):
     global sequence_num
@@ -255,7 +276,7 @@ def convert_to_notebook(pre_examples_content, parsed_json, post_examples_content
         "nbformat_minor": 2
     }
 
-    notebook_path = "test.ipynb"
+    notebook_path = "wtf.ipynb"
 
     result["cells"] += convert_to_cells([generate_markdown_block(pre_examples_content)], False)
 
@@ -328,4 +349,5 @@ with open(fpath, 'r+', encoding="utf-8") as f:
 
     except StopIteration as e:
         #pprint.pprint(result, indent=2)
+        pre_stuff.append(HOSTED_NOTEBOOK_INSTRUCTIONS)
         convert_to_notebook(pre_stuff, result, post_stuff)
